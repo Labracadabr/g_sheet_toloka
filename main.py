fff77d0c9f91804d8fa72e4e475764cdc250b9a0
Dmitrii <dminokin@gmail.com>
2023-12-04 17:34:11 +0300

    init

--- main.py
+++ main.py
@@ -15,14 +15,10 @@ def main():
 # время запуска
 upd_hour, upd_minute = 23, 59
 schedule.every().day.at(f"{upd_hour:02d}:{upd_minute:02d}").do(main)
-s = start_time = 0
 
 # поллинг каждые n секунд
 n = 30
 while 1:
+    # проверить, не настало ли время
     schedule.run_pending()
-    if s < 1:
-        start_time = int(datetime.now().replace(hour=upd_hour, minute=upd_minute, second=0).timestamp())
-    s = start_time - int(time.time())
-    print('\rСтарт через', s, 'сек', end='')
     time.sleep(n)
