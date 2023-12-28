ef3face0ebe4671adcd86a9c0338995cbbecd885
Dmitrii <dminokin@gmail.com>
2023-12-28 14:17:39 +0300

    изм расписание

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for hour in update_hours:
     schedule.every().day.at(f"{hour:02d}:00").do(accounts_update)
 
 # скрипт поменьше - запускать каждые 24 ч
-schedule.every().day.at("02:50").do(pools)
+schedule.every().day.at("23:55").do(pools)
 
 print('Сейчас', datetime.now().strftime("%H:%M:%S"))
 
