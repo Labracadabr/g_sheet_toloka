25ea269b794055e808567ae70eb1729dc0736c0d
Dmitrii <dminokin@gmail.com>
2024-01-29 22:45:17 +0300

    учет часового пояса устройства

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ from new_pools import pools
 from all_accounts import accounts_update
 
 
-# большой скрипт - запускать неск раз в день
-update_hours = [10, 18]
-for hour in update_hours:
-    schedule.every().day.at(f"{hour:02d}:00").do(accounts_update)
+# корректировка по часовому поясу. например при gmt+3 превратить 22 в '01'
+def gmt_shift(hour: int) -> str:
+    gmt = int(-(time.timezone if (time.localtime().tm_isdst == 0) else time.altzone) / 60 / 60)  # часовой пояс компа
+    hour = str((hour + gmt) % 24).zfill(2)
+    return hour
 
-# скрипт поменьше - запускать каждые 24 ч
-schedule.every().day.at("23:55").do(pools)
+
+# большой скрипт - запускать неск раз в день
+update_hours = [7, 15, 19]  # часы указать в gmt 0
+for h in update_hours:
+    t = f"{gmt_shift(h)}:05"
+    schedule.every().day.at(t).do(accounts_update)
+
+# скрипт поменьше - запускать каждые 24 ч в полночь по gmt 0
+t = f"{gmt_shift(23)}:55"
+schedule.every().day.at(t).do(pools)
 
 print('Сейчас', datetime.now().strftime("%H:%M:%S"))
 
 # поллинг каждые n секунд
-n = 20
+n = 2
 while 1:
     # проверить, не настало ли время
     schedule.run_pending()
